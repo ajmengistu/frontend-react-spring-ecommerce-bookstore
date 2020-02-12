c45a196aa94520cecbd7be1cc7586d243c263ad0
ajmengistu <a_mengistu@outlook.com>
2020-02-12 14:06:43 -0500

    Updated Jenkinsfile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import User from "./components/User";
 // export const url = "https://jsonplaceholder.typicode.com/users";
-export const url = "http://localhost:8080/greeting";
+export const url = "http://localhost:8081/greeting";
 const App = () => {
   return (
     <div className="App">
