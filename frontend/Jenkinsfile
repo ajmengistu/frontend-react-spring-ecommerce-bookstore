3e923886980984514a3632a3ef21323c51a21302
ajmengistu <a_mengistu@outlook.com>
2020-02-12 19:14:39 -0500

    Added .travis.yml file for Travis CI. Updated App.js with Spring backend url app on Heroku. Moved Jnekinsfile from root project directory to frontend directory

---
+++ frontend/Jenkinsfile
@@ -0,0 +1,13 @@
+pipeline {
+    stages {
+        stage('test node version') {
+            steps {
+                echo '-- Testing node version --'
+                sh 'node --version'
+                sh 'which node'
+                // npm install
+                // npm test
+            }
+        }
+    }
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.css";
 import User from "./components/User";
 // export const url = "https://jsonplaceholder.typicode.com/users";
-export const url = "http://localhost:8081/greeting";
+// export const url = "http://localhost:8081/greeting";
+export const url = "https://bookstore-react-spring.herokuapp.com/greeting"
 const App = () => {
   return (
     <div className="App">
