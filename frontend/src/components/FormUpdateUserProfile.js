704ff5f88f11230f678347386df299283a41d459
ajmengistu <a_mengistu@outlook.com>
2020-03-09 19:14:26 -0400

    Updated Error component page to include an appropriate message when a client/user requests an invalid resource/url.

--- frontend/src/components/FormUpdateUserProfile.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-// A form to update user profile.
-const FormUpdateUserProfile = () => {
-  return <></>;
-};
--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-// All other invalid url requests will be shown this page.
+// All other invalid url requests will be shown this page. 404 page.
 const Error = () => {
-  return <div> Hello from Error page. </div>;
+  return (
+    <div className="col-sm-6 container mt-4 alert alert-danger">
+      The page you requested does not exist. Please 
+      <Link to="/signin"> Sign in</Link> or <br></br>
+      go to the <Link to="/">home page</Link>.
+    </div>
+  );
 };
 
 export default Error;
