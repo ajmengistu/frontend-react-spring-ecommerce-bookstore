c1fd65696cc17f36c02241ae8de2f7508eb0a7fa
ajmengistu <a_mengistu@outlook.com>
2020-03-09 14:45:11 -0400

    Added a feature where the Home.js component renders data that it fetches from a remote api to make sure the frontend/client is able to make calls to a remote api during production.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import Footer from "../components/Footer";
+import { resolve } from "../utils/resolve";
 
 // Home page.
 const Home = () => {
+  const [users, setUsers] = useState([{ name: "Error" }]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await resolve(
+        axios
+          .get("https://jsonplaceholder.typicode.com/users")
+          .then(response => response)
+      );
+      const data = response.data.data;
+      setUsers(data);
+    };
+    getUsers();
+  }, []);
   return (
     <>
-      <div className="container mt-4">This is the home page!</div>
-      ;
+      <div className="container mt-4">
+        <h4> This is the home page!</h4>
+        <div>
+          <ul data-testid="user-name">
+            {users.length === 0 ? (
+              <p data-testid="loading">Loading....</p>
+            ) : (
+              users.map((user, index) => <li key={index}> {user.name} </li>)
+            )}
+          </ul>
+        </div>
+      </div>
+
       <Footer />
     </>
   );
