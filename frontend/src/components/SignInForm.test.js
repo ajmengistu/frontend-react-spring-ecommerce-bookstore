137b9b73fc2aa1293bdc49ee496059c265e54c81
ajmengistu <a_mengistu@outlook.com>
2020-03-12 19:31:49 -0400

    Deleted User component and its tests because it longer needed for the app. Added tests for SignInForm.

---
+++ frontend/src/components/SignInForm.test.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { render, cleanup, waitForElement } from "@testing-library/react";
+import axiosMock from "axios";
+
+// Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
+// unmount and cleanup DOM after the test is finished.
+afterEach(() => {
+  axiosMock.get.mockClear();
+  cleanup;
+});
+
+jest.mock("axios");
+
+test("Example test", () => {
+  expect(true).toBeTruthy();
+});
+
+test("Example test", () => {
+  expect(false).toBeFalsy();
+});
+
+
+// const mockCall = () => {
+//   axiosMock.get.mockResolvedValueOnce({
+//     data: [
+//       {
+//         firstName: "John"
+//       },
+//       {
+//         firstName: "Thomas"
+//       }
+//     ]
+//   });
+// };
+
+// describe("Test <User /> component", () => {
+//   test("Show loader when it's fetching data from api", () => {
+//     const url = "/users";
+//     mockCall();
+
+//     const { getByText, getAllByText } = render(<User url={url} />);
+//     expect(getByText(/Loading..../)).toBeInTheDocument;
+//   });
+
+//   test("Successfully fetching data from axios mock api", async () => {
+//     const url = "/users";
+//     mockCall();
+
+//     const { getByText, getAllByText } = render(<User url={url} />);
+
+//     await waitForElement(() => getAllByText("Thomas"));
+//     expect(axiosMock.get).toHaveBeenCalledTimes(1);
+//     expect(axiosMock.get).toHaveBeenCalledWith(url);
+//   });
+// });
